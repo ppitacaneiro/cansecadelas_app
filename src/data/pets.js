3fe93c8227f99759c2c022f410b8db1cb38b22d3
Pablo Pita <ppitacaneiro@gmail.com>
2024-03-23 10:07:41 +0100

    fake data for pets

---
+++ src/data/pets.js
@@ -0,0 +1,46 @@
+const pets = [
+    {
+        name: 'Max',
+        shelter: 'Shelter 1',
+        created_at: '2021-01-01',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+    {
+        name: 'Buddy',
+        shelter: 'Shelter 2',
+        created_at: '2021-01-02',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+    {
+        name: 'Charlie',
+        shelter: 'Shelter 3',
+        created_at: '2021-01-03',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+    {
+        name: 'Jack',
+        shelter: 'Shelter 4',
+        created_at: '2021-01-04',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+    {
+        name: 'Milo',
+        shelter: 'Shelter 5',
+        created_at: '2021-01-05',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+    {
+        name: 'Bailey',
+        shelter: 'Shelter 6',
+        created_at: '2021-01-06',
+        url_image: 'https://i2.wp.com/refugiocambados.es/wp-content/uploads/2024/02/russ4-min-scaled.jpg',
+        url: 'https://www.google.com',
+    },
+];
+
+export default pets;
